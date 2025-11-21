db073c00ef4501e89b97e786bf0ad3206fbfe2c6
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2025-11-21 00:01:41 -0800

    database

---
+++ src/com/example/school/database/Database.java
@@ -0,0 +1,14 @@
+package com.example.school.database;
+
+import com.example.school.models.Student;
+import com.example.school.models.Teacher;
+import com.example.school.models.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static final List<Student> students = new ArrayList<>();
+    public static final List<Teacher> teachers = new ArrayList<>();
+    public static final List<Course> courses = new ArrayList<>();
+}
